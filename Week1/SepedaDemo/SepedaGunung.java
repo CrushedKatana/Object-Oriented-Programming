06b36ff80d283d0e51e84c1b9103fdddf48f7509
CrushedKatana <charelsadewo123@gmail.com>
2024-08-30 09:39:51 +0700

    commit 1

---
+++ Week1/SepedaDemo/SepedaGunung.java
@@ -0,0 +1,12 @@
+package SepedaDemo;
+
+public class SepedaGunung {
+private String tipeSuspensi;
+
+public void setTipeSuspensi(String newValue){
+    tipeSuspensi =  newValue;
+}
+public void cetakStatus(){
+    
+}
+}
