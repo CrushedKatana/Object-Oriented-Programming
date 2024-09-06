2766f7520efb405831c71ffde7564ccf081e4607
CrushedKatana <charelsadewo123@gmail.com>
2024-09-06 10:19:58 +0700

    commit

--- Week2/TestMahasiswa.java
+++ Week2/TestMahasiswa.java
@@ -8,5 +8,19 @@ public class TestMahasiswa {
         mhs1.alamat="Jl.Vinolia No 1A";
         mhs1.kelas="1A";
         mhs1.tampilBiodata();
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.nim = 102;
+        mhs2.nama = "Budi";
+        mhs2.alamat = "Jl.Mawar No 2B";
+        mhs2.kelas = "1B";
+        mhs2.tampilBiodata();
+
+        Mahasiswa mhs3 = new Mahasiswa();
+        mhs3.nim = 103;
+        mhs3.nama = "Siti";
+        mhs3.alamat = "Jl.Melati No 3C";
+        mhs3.kelas = "1C";
+        mhs3.tampilBiodata();
     }
 }
