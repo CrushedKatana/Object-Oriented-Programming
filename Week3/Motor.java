16ed39ba689ef911778c50b3efdd67b83c0beb94
CrushedKatana <charelsadewo123@gmail.com>
2024-09-13 09:13:53 +0700

    com

--- Week3/Motor.java
+++ Week3/Motor.java
@@ -1,15 +1,38 @@
 package Week3;
 
 public class Motor {
-    private public int speed = 0 ;
-    private public boolean motorOn=false;
+    private  int speed = 0 ;
+    private boolean motorOn=false;
 
     public void startEngine(){
         motorOn=true;
     }
 
     public void turnOffEngine(){
-        motorOn=F
+        motorOn=false;
+        speed=0;
+    }
+
+    public void increaseEngine(){
+        if (motorOn == true){
+            if (speed + 5 <= 100) { 
+                speed += 5;
+            } else {
+                speed = 100; 
+                System.out.println("Maximum speed reached!");
+            }
+        } else {
+            System.out.println("Motor cycle Off");
+        }
+    }
+    
+
+    public void reduceEngine(){
+        if (motorOn==true){
+            speed=-5;
+        }else{
+            System.out.println("Motor cycle Off");
+        }
     }
 
     public void statusPrint(){
