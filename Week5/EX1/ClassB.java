284137a6ccac660d239d4a037d2e476570b0f6ec
CrushedKatana <charelsadewo123@gmail.com>
2024-10-04 09:03:58 +0700

    commit

---
+++ Week5/EX1/ClassB.java
@@ -0,0 +1,13 @@
+package Week5.EX1;
+
+public class ClassB {
+    public int z;
+
+    public void getNilaiZ(){
+        System.out.println("nilai Z: "+ z);
+    }
+
+    public void getJumlah (){
+        System.out.println("jumblah "+ (x+y+z) );
+    }
+}
---
+++ Week5/EX1/Percobaaan1.java
@@ -0,0 +1,14 @@
+package Week5.EX1;
+
+public class Percobaaan1 {
+    
+    public static void main(String[] args) {
+        ClassB hitung = new ClassB();
+            hitung.x=20;
+            hitung.y=30;
+            hitung.z=5;
+            hitung.getNilai();
+            hitung.getNilaiZ();
+            hitung.getJumlah();   
+    }
+}
