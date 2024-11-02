33d6492c3d86d22f2636c673741899724c68f617
CrushedKatana <charelsadewo123@gmail.com>
2024-11-03 00:30:58 +0700

    Update Rector.java

--- Week7/EX2/Rector.java
+++ Week7/EX2/Rector.java
@@ -1,13 +1,22 @@
 package Week7.EX2;
 
 public class Rector {
-    public void giveCumlaudeCertificate (ICumlaude student) {
-    System.out.println("I am a Rector, give a Cumlaude Certificate");
-    System.out.println("Congratulations! Please introduce yourself..");
 
-    student.graduate();
-    student.getHighGPA();
-    
-    System.out.println("-----------------------------------------------");
+    public void giveCumlaudeCertificate(Student student) {
+        System.out.println("I am a Rector, give a Cumlaude Certificate");
+        System.out.println("Congratulations! Please introduce yourself..");
+
+        student.studyInCampus();
+
+        if (student instanceof ICumlaude) {
+            ICumlaude cumlaudeStudent = (ICumlaude) student; 
+            cumlaudeStudent.graduate();
+            cumlaudeStudent.getHighGPA();
+        } else {
+            System.out.println("My GPA is 3,1");
+        }
+        
+        System.out.println("-----------------------------------------------");
     }
 }
+
